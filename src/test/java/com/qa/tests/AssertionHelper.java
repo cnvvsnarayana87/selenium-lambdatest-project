0bb05b62aa290d0f9c1c8b466cf3d1c3b9eb8268
V V Satyanarayana Nallam <cnvvsnarayana87@gmail.com>
2024-04-13 17:20:43 +0530

    Selenium Lambdatest Project

--- src/test/java/com/qa/tests/AssertionHelper.java
+++ src/test/java/com/qa/tests/AssertionHelper.java
@@ -1,16 +1,35 @@
 package com.qa.tests;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 public class AssertionHelper {
-	public static void assertAndReport(ExtentTest test, String actualValue, String expectedValue, String testName) {
+	public static void assertAndReport(ExtentTest test, String actualValue, String expectedValue, String testName,
+			WebDriver driver) throws IOException {
 		if (actualValue.equals(expectedValue)) {
 			test.pass(testName + " completed successfully");
 		} else {
 			test.fail(testName + " failed.");
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath(captureScreenshot(driver)).build());
 		}
 		Assert.assertEquals(actualValue, expectedValue);
+	}
+
+	public static String captureScreenshot(WebDriver driver) throws IOException {
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destFile = new File(
+				System.getProperty("user.dir") + "\\target\\screenshots\\" + System.currentTimeMillis() + ".png");
+		String errFilePath = destFile.getAbsolutePath();
+		FileUtils.copyFile(srcFile, destFile);
+		return errFilePath;
 	}
 }
--- src/test/java/com/qa/tests/LambdaTestECommerceTests.java
+++ src/test/java/com/qa/tests/LambdaTestECommerceTests.java
@@ -1,5 +1,7 @@
 package com.qa.tests;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -21,41 +23,44 @@ public class LambdatestECommerceTests extends DriverBase {
 
 	@BeforeClass
 	public void setUpExtentReports() {
-		ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/reports/Spark.html");
 		extent = new ExtentReports();
 		extent.attachReporter(spark);
 	}
 
 	@Test
-	public void testLoadHomePage() {
+	public void testLoadHomePage() throws IOException {
 		this.driver = getDriver();
 		driver.manage().window().maximize();
 		driver.get(config.getProperty("url"));
 		test = extent.createTest("Validate Home Page", "This test validates whether Home page is loaded or not");
 		pageWaitHelper.pageImplicitlyWait(driver);
 		String actualTitle = driver.getTitle();
-		AssertionHelper.assertAndReport(test, actualTitle, config.getProperty("homepageTitle"), "testLoadHomePage");
+		AssertionHelper.assertAndReport(test, actualTitle, config.getProperty("homepageTitle"), "testLoadHomePage",
+				driver);
 	}
 
 	@Test
-	public void testAccountLogin() {
+	public void testAccountLogin() throws IOException {
 		HomePage homePage = new HomePage(driver);
 		homePage.clickOnLoginButton();
 		pageWaitHelper.pageImplicitlyWait(driver);
 		String actualTitle = driver.getTitle();
 		test = extent.createTest("Validate Login Page", "This test validates whether Login page is loaded or not");
-		AssertionHelper.assertAndReport(test, actualTitle, config.getProperty("accountLoginPageTitle"), "testAccountLogin");
+		AssertionHelper.assertAndReport(test, actualTitle, config.getProperty("accountLoginPageTitle"),
+				"testAccountLogin", driver);
 	}
 
 	@Test
-	public void testLoginProcess() {
+	public void testLoginProcess() throws IOException {
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.performLogin(config.getProperty("username"), config.getProperty("password"));
 		pageWaitHelper.pageImplicitlyWait(driver);
 		String actualTitle = driver.getTitle();
 		test = extent.createTest("Validate Login Process",
 				"This test validates whether user can able to Login with valid credentials");
-		 AssertionHelper.assertAndReport(test, actualTitle, config.getProperty("loginPageTitle"), "testLoginProcess");
+		AssertionHelper.assertAndReport(test, actualTitle, config.getProperty("loginPageTitle"), "testLoginProcess",
+				driver);
 	}
 
 	@AfterClass
